96f3e5248fb3da8c20070e8558b7627e3d8d0781
dj0nny <frank96990@gmail.com>
2023-01-07 14:21:39 +0100

    generate app and add daisyUI

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+</script>
+
+<header class="navbar bg-primary text-primary-content flex justify-center items-center">
+	<a href="/" class="btn btn-ghost normal-case text-2xl">Sveltekit Playground</a>
+</header>
+<main class="container mx-auto my-11 text-center">
+	<slot />
+</main>
